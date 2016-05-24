63623320c4b8acf9fb6aa3d58ced347aec44f615
Rasmus Nørskov <rasmus@norskov.com>
2016-05-25 00:35:41 +0200

    changes to system functions

--- src/functions.js
+++ src/functions.js
@@ -1,82 +1,115 @@
-var drone = require('ar-drone');
-var client = drone.createClient();
+module.exports = function (client, navdata) {
+    console.log(navdata);
 
-module.exports = {
-    takeoff: function (params, callback) {
+    function options(param, callback) {
+        console.log("setting options");
+        switch (param) {
+            case "name":
+                console.log("settings name");
+                client.options('NETWORK:ssid_single_player', param.name);
+                break;
+            case "outdoor":
+                client.options('CONTROL:outdoor', param.outdoor);
+                break;
+        }
+    }
+
+    function takeoff(param, callback) {
         client.takeoff();
         client.after(5000, function () {
             if (typeof callback === "function") callback();
         });
-    },
+    }
 
-    land: function (params, callback) {
+    function land(param, callback) {
         client.land();
         setTimeout(function () {
             client.stop();
             if (typeof callback === "function") callback();
         }, 5000);
-    },
+    }
 
-    fly: function (params, callback) {
-        if (params.time == undefined)
+    function fly(param, callback) {
+        if (param.time == undefined)
             throw "time is a required parameter";
-        if (params.speed == undefined)
+        if (param.speed == undefined)
             throw "speed is a required parameter";
-        if (params.direction == undefined)
+        if (param.direction == undefined)
             throw "direction is a required parameter";
 
-        params.speed = params.speed / 100;
-        params.time = params.time * 1000;
+        var speed = param.speed / 100;
+        var time = param.time * 1000;
 
-        switch (params.direction) {
+        switch (param.direction) {
             case "front":
-                client.front(params.speed);
+                client.front(speed);
                 break;
             case "back":
-                client.back(params.speed);
+                client.back(speed);
                 break;
             case "left":
-                client.left(params.speed);
+                client.left(speed);
                 break;
             case "right":
-                client.right(params.speed);
+                client.right(speed);
                 break;
             case "up":
-                client.up(params.speed);
+                client.up(speed);
                 break;
             case "down":
-                client.down(params.speed);
+                client.down(speed);
                 break;
         }
 
         setTimeout(function () {
             client.stop();
             if (typeof callback === "function") callback();
-        }, params.time)
-    },
+        }, time)
+    }
 
-    rotate: function (params, callback) {
-        if (params.time == undefined)
+    function rotate(param, callback) {
+        if (param.time == undefined)
             throw "time is a required parameter";
-        if (params.speed == undefined)
+        if (param.speed == undefined)
             throw "speed is a required parameter";
-        if (params.direction == undefined)
+        if (param.direction == undefined)
             throw "direction is a required parameter";
 
-        params.speed = params.speed / 100;
+        var speed = param.speed / 100;
+        var time = param.time * 1000;
 
-        switch (params.direction) {
+        switch (param.direction) {
             case "cw":
-                client.clockwise(params.speed);
+                client.clockwise(speed);
                 break;
             case "ccw":
-                client.counterClockwise(params.speed);
+                client.counterClockwise(speed);
                 break;
         }
 
         setTimeout(function () {
             client.stop();
             if (typeof callback === "function") callback();
-        }, params.time)
+        }, time)
+    }
+
+    function animate(param, callback) {
+        var animation = param.animation;
+        var time = param.time * 1000;
+
+        client.animate(animation, time);
+        setTimeout(function () {
+            client.stop();
+            if (typeof callback === "function") callback();
+        }, time)
+    }
+
+    return {
+        options: options,
+        takeoff: takeoff,
+        land: land,
+        fly: fly,
+        rotate: rotate,
+        animate: animate
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-var functions = require('../src/functions');
-require('../tmp/compiled')(functions);
-setup();
-route();
+var drone = require('ar-drone');
+var client = drone.createClient();
+
+client.once('navdata', function (navdata) {
+    var functions = require('./functions')(client, navdata);
+    require('../tmp/compiled')(functions);
+    route();
+    setup();
+});
+
